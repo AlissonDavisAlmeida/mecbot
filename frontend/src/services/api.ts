1adcc09dd6d17f3048e4679f27526a7a6798daf6
Alisson <alissoneatena1@gmail.com>
2026-04-30 19:04:49 -0300

    feat: mudando nome da variavel da api no .env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: `http://localhost:${process.env.API_PORT ?? 4000}`, // backend NestJS
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? `http://localhost:${process.env.API_PORT ?? 4000}`, // backend NestJS
   withCredentials: true,
 })
